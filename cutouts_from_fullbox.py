2b8b603038a69f38b8411078271a5f75545c7583
milchada <uchadaya@gmail.com>
2023-10-26 13:48:16 +0200

    Make cutouts, write FITS

--- cutouts_from_fullbox.py
+++ cutouts_from_fullbox.py
@@ -77,100 +77,23 @@ def make_proj(filename, snapnum, halonum, dm=True, gas=True, ns=["x","y","z"], s
             p.writeto('dm_massproj_%s_%s_%s%s.fits' % (snapnum, halonum, n, suffix), overwrite=True)
             del(p)
             gc.collect()
-         
-   #now select only those that are valid in all fields, right?
-   allmask = (np.sum(dmmass, axis = 1) > 0) * (np.sum(dmvel, axis = 1) > 0) * (np.sum(sb, axis = 1) > 0) 
-            * (np.sum(kt, axis = 1) > 0) * (np.sum(bh, axis = 1) > 0)
-   dmmass = dmmass[allmask]
-   dmvel  = dmvel[allmask]
-   sb     = sb[allmask]
-   kt     = kt[allmask]
-   bh     = bh[allmask]
-
-   np.save('dmmass.npy', dmmass)
-   np.save('dmvel.npy', dmvel)
-   np.save('sb.npy', sb)
-   np.save('kT.npy', kt)
-   np.save('bhmdot.npy', bh)
 
 def compile_training_data():
    import glob
-   dm = glob.glob('dm_Mass_*npy'); dm.sort() 
-   gas = [d.replace('dm_Mass','gas_Density') for d in dm]
-   temp = [d.replace('dm_Mass','gas_Temperature') for d in dm]
-   bh = [d.replace('dm_Mass','bh_Mdot') for d in dm]
-   inputs = np.zeros((len(dm), 512, 512, 2))
-   outputs = np.zeros((len(dm), 512, 512, 2))
+   fdm = glob.glob('dm_Mass_*npy'); dm.sort() 
+   fgas = [d.replace('dm_Mass','gas_Density') for d in dm]
+   ftemp = [d.replace('dm_Mass','gas_Temperature') for d in dm]
+   fbh = [d.replace('dm_Mass','bh_Mdot') for d in dm]
+   dm = np.zeros((len(dm), 512, 512, 1))
+   bh = np.zeros((len(dm), 512, 512, 1))
+   temp = np.zeros((len(dm), 512, 512, 1))
+   gas = np.zeros((len(dm), 512, 512, 1))
    for i in range(len(inputs)):
-       inputs[i, :,:,0] = np.load(dm[i])
-       inputs[i, :,:,1] = np.load(bh[i])
-       outputs[i, :,:,0] = np.load(gas[i])
-       outputs[i, :,:,1] = np.load(bh[i])
-   np.save('dm_bhar.npy', inputs)
-   np.save('gasrho_temp.npy', outputs)
-
-   def renorm(arr, eps=0.1, log=True):
-       if log:
-           arr = np.log10(arr)
-           arr[arr == -np.inf] = np.nan
-       arr -= np.nanmin(arr)
-       arr += eps
-       arr /= np.nanmax(arr)
-       return arr
-
-   inputs[:,:,:,0] = renorm(inputs[:,:,:,0])
-   inputs[:,:,:,1] = renorm(inputs[:,:,:,1])
-   outputs[:,:,:,0] = renorm(outputs[:,:,:,0])
-   outputs[:,:,:,1] = renorm(outputs[:,:,:,1])
-   gc.collect()
-   inputs[np.isnan(inputs)] = 0
-   outputs[np.isnan(outputs)] = 0
-   np.save('inputs.npy', inputs)
-   np.save('outputs.npy', outputs)
-
-def get_group_props(matches, basePath = '/n/holystore01/LABS/hernquist_lab/Lab/IllustrisTNG/Runs/L205n2500TNG/output/'):
-   from astropy import units as u
-   import gc
-   import pandas as pd 
-   import numpy as np 
-
-   if 'DM' in basePath:
-      fields = ['Group_M_Crit200','GroupNsubs','Group_R_Crit200', 'GroupPos']
-      fieldnames = ['Group_M_Crit200','GroupNsubs','Group_R_Crit200', 'GroupPos']
-   else:
-      fields = ['Group_M_Crit200', 'GroupBHMass', 'GroupBHMdot','GroupNsubs', 'GroupSFR', 'Group_R_Crit200', 'GroupPos']
-      fieldnames = ['Group_M_Crit200', 'GroupBHMass', 'GroupBHMdot','GroupNsubs', 'GroupSFR', 'Group_R_Crit200', 'GroupPos'] 
-   if 'GroupPos' in fields:
-      groupcat = np.zeros((len(matches), len(fields)+4))
-      fieldnames[-1] = 'GroupPosX'
-      fieldnames += ['GroupPosY', 'GroupPosZ']
-   else:
-      groupcat = np.zeros((len(matches), len(fields)+2))
-   i=0
-   for snap in [50,67,78,99]:#np.unique(todo[:,0]):
-      sub = matches[matches[:,0] == snap]
-      if 'DM' in basePath:
-         hnums = sub[:,2].astype(int)
-      else:
-         hnums = sub[:,1].astype(int)
-      cat = il.groupcat.loadHalos(basePath,int(snap),fields=fields)
-      groupcat[i:i+len(hnums), 0] = snap 
-      groupcat[i:i+len(hnums), 1] = hnums
-      for field in fields:
-         try:
-            groupcat[i:i+len(hnums), 2+fields.index(field)] = cat[field][hnums]
-         except ValueError: #this will happen for GroupPos
-            groupcat[i:i+len(hnums), 2+fields.index(field):5+fields.index(field)] = cat[field][hnums]
-      i+=len(sub)
-      del(cat)
-      gc.collect()
-      print(int(snap), 'done')
-   
-   df = pd.DataFrame(data = groupcat, columns=['snap', 'halo']+fieldnames)
-   h =  il.groupcat.loadHeader(basePath, snapNum=int(snap))['HubbleParam']
-   munit = 1e10*u.Msun/h
-   if 'DM' not in basePath:
-      df['GroupBHMdot'] *= 1e10*u.Msun/(0.978*u.Gyr)
-      df['GroupBHMass'] *= munit
-   df['Group_M_Crit200'] *= munit
-   return df
+       dm[i, :,:,0] = np.load(fdm[i])
+       bh[i, :,:,0] = np.load(fbh[i])
+       temp[i, :,:,0] = np.load(ftemp[i])
+       gas[i, :,:,0] = np.load(fgas[i])
+   np.save('dm.npy', dm)
+   np.save('bh.npy', bh)
+   np.save('temp.npy', temp)
+   np.save('rho.npy', rho)
